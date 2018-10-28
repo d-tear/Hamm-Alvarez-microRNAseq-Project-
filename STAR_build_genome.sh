446805e39d008bc8a8effc173e10dc8846aef30a
David <tyrpak@usc.edu>
2018-10-27 17:00:21 -0700

    removed --genomeChrBinNbits to restore settings to default. With the lbuntu desktop environemnt I should have more RAM. Also increased the number of threads to 8 to test out hyperthreading capabilities.

--- STAR_build_genome.sh
+++ STAR_build_genome.sh
@@ -3,11 +3,10 @@
 
 #####This script uses STAR to build the mouse geome index#####
 
-#Note that --genomeChrBinNbits 15 to reduce RAM consumption. I believe the default is 18
+#Note that --genomeChrBinNbits can be reduced reduce RAM consumption. I believe the default is 18
 #Note that sjdbOverhang should be set as readlength -1. Because I trimmed the Reads down to 30bp this value is 29
 
-STAR --runThreadN 6 --runMode genomeGenerate \
---genomeChrBinNbits 15 \
+STAR --runThreadN 8 --runMode genomeGenerate  \
 --genomeDir /media/david/Main/David/Genomes/mouse/STAR/Genome_Indexes \
 --genomeFastaFiles /media/david/Main/David/Genomes/mouse/STAR/GRCm38.primary_assembly.genome.fa \
 --sjdbGTFfile /media/david/Main/David/Genomes/mouse/STAR/gencode.vM19.primary_assembly.annotation.gtf \
